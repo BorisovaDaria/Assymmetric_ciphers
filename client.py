2335862be4a124a8538bacebe23099931f099b04
BorisovaDaria <dashabborisova@gmail.com>
2021-12-19 06:11:01 +0300

    initial commit

--- client.py
+++ client.py
@@ -1,14 +1,37 @@
 import socket
 import pickle
 
-HOST = '127.0.0.1'
+ALPHA = u'абвгдеёжзийклмнопрстуфхцчшщьъэюя'
+
+
+def encode(text, step):
+    return text.translate(
+        str.maketrans(ALPHA, ALPHA[step:] + ALPHA[:step]))
+
+
+def decode(text, step):
+    return text.translate(
+        str.maketrans(ALPHA[step:] + ALPHA[:step], ALPHA))
+
+HOST = 'localhost'
 PORT = 8080
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+p, g, a = 7, 52, 31
 A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+sock.send(pickle.dumps((g, p, A)))
+B = pickle.loads(sock.recv(1024))
+K = B ** a % p
+print("K =", K)
+
+msg = "Привет!"
+msg = encode(msg, K)
+sock.send(pickle.dumps(msg))
+
+msg = pickle.loads(sock.recv(1024))
+print(msg)
+print(decode(msg, K))
 
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,42 @@
 import socket
 import pickle
 
-HOST = '127.0.0.1'
+ALPHA = u'абвгдеёжзийклмнопрстуфхцчшщьъэюя'
+
+
+def encode(text, step):
+    return text.translate(
+        str.maketrans(ALPHA, ALPHA[step:] + ALPHA[:step]))
+
+
+def decode(text, step):
+    return text.translate(
+        str.maketrans(ALPHA[step:] + ALPHA[:step], ALPHA))
+
+HOST = 'localhost'
 PORT = 8080
 
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
-conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+b = 81
+
+conn, addr = sock.accept()
+g, p, A = pickle.loads(conn.recv(1024))
+B = g ** b % p
+K = A ** b % p
+print(g, p, A)
+print("K =", K)
+
+conn.send(pickle.dumps(B))
+
+msg = pickle.loads(conn.recv(1024))
+print(msg)
+print(decode(msg, K))
+
+msg = "И тебе привет!"
+msg = encode(msg, K)
+conn.send(pickle.dumps(msg))
 
 conn.close()
